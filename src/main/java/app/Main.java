bd5ebfce97128d6669e8de77db4053b268b57c4e
themoebius <moebiusmethod@outlook.com>
2021-07-06 00:34:58 +0200

    test-cmds removed

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,30 +9,5 @@ public class Main {
 
         run(args);
 
-        /*
-        ArrayList<Stock> generatedStocks = new ArrayList<>();
-
-        ArrayList<String[]> stocksAndSubredditsSplit = splitStocksFromSubreddits(args);
-
-        String[] searchTerms = stocksAndSubredditsSplit.get(0);
-        String[] subRedditChoice = stocksAndSubredditsSplit.get(1);
-
-
-        String searchTerm = searchTerms[0];
-        Stock testStock = new Stock(searchTerm, subRedditChoice);
-
-        System.out.println(testStock.toString());
-        System.out.println("-------------------");
-*/
-
-        /*
-        //generating Stock-Objects from stockNames in run-config
-        //and scraping information to fill Objects
-        ArrayList<Stock> getData = createRunConfig(args);
-
-        // converting and saving &| appending generated Objects
-        // to .txt files in 'generatedData'- folder
-        saveAllToFile(getData);
-*/
     }
 }
