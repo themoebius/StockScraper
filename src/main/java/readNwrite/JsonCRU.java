c1c13c5a70ca87fbfbd44fe230c1ecc77b92cfff
themoebius <moebiusmethod@outlook.com>
2021-07-10 20:56:15 +0200

    implemented writing/reading last runtime to/from .txt-file

--- src/main/java/readNwrite/JsonCRU.java
+++ src/main/java/readNwrite/JsonCRU.java
@@ -3,12 +3,16 @@ package readNwrite;
 import app.Stock;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+
+import static java.nio.file.Files.exists;
 
 public class JsonCRU {
 
@@ -27,7 +31,7 @@ public class JsonCRU {
         // find position of separator '!' to split stockName from jsonString
         int separatorPosition = singleJsonStr.indexOf('!');
 
-        String stockName = singleJsonStr.substring(0 , separatorPosition);
+        String stockName = singleJsonStr.substring(0, separatorPosition);
         String cleanJsonStr = singleJsonStr.substring(separatorPosition + 1, singleJsonStr.length() - 1);
 
         int success = 0;
@@ -36,13 +40,53 @@ public class JsonCRU {
         // if no file exists, file will be created.
         try {
             final Path path = Paths.get(".\\generatedData\\" + stockName + ".txt");
-            Files.write(path, Arrays.asList(cleanJsonStr), StandardCharsets.UTF_8, Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
-            success =1;
+            Files.write(path, Arrays.asList(cleanJsonStr), StandardCharsets.UTF_8, exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+            success = 1;
         } catch (IOException e) {
             System.out.println("Something went wrong here..." + e);
         }
         if (success != 0) {
             System.out.println("saving data for " + stockName + " to file successful");
+        }
+    }
+
+    public static String createJsonStringFromDateObject(LocalDateTime dateTime) {
+        Gson gson = new Gson();
+        return gson.toJson(dateTime);
+    }
+
+    public static LocalDateTime createDateObjectFromJsonString(String jsonStr) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonStr, LocalDateTime.class);
+    }
+
+
+    public static LocalDateTime readDateFromFile(String filePath) throws IOException {
+        final Path path = Paths.get(filePath);
+        boolean saveFileExists = Files.exists(path);
+        if (saveFileExists) {
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            Gson gson = new Gson();
+            return gson.fromJson(reader, LocalDateTime.class);
+        } else {
+            System.out.println("a new file will be created to keep track of the time-periods between runs");
+            return null;
+        }
+    }
+
+    public static void writeDateToFile(String DateAsJsonString) {
+        int success = 0;
+        // save jsonString to file: if file exists, it will be cleared,
+        // if no file exists, file will be created.- the file will ever only hold one LocalDateTime Json.
+        try {
+            final Path path = Paths.get(".\\generatedData\\" + "ScrapeTimeTracker" + ".txt");
+            Files.write(path, Arrays.asList(DateAsJsonString), StandardCharsets.UTF_8, exists(path) ? StandardOpenOption.TRUNCATE_EXISTING : StandardOpenOption.CREATE);
+            success =1;
+        } catch (IOException e) {
+            System.out.println("Something went wrong here..." + e);
+        }
+        if (success != 0) {
+            System.out.println("saving DateTime of current run to file successful");
         }
     }
 }
