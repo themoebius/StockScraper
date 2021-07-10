5b09e5ce54fe2973685727ad4b26763f9c116b9d
themoebius <moebiusmethod@outlook.com>
2021-07-10 17:20:34 +0200

    check for Weekends/Holidays implemented

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -5,6 +5,9 @@ import app.Stock;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Scanner;
+
 import static java.util.Arrays.copyOfRange;
 import static readNwrite.JsonCRU.createJsonStringFromObject;
 import static readNwrite.JsonCRU.writeJsonStringToFile;
@@ -37,21 +40,44 @@ public class Helper {
     }
 
     public static void run(String[] args) throws IOException {
-        ArrayList<Stock> generatedStocks = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
 
-        ArrayList<String[]> stocksAndSubredditsSplit = splitStocksFromSubreddits(args);
+        if (isInTimeWindow(calendar)) {
+            ArrayList<Stock> generatedStocks = new ArrayList<>();
+            ArrayList<String[]> stocksAndSubredditsSplit = splitStocksFromSubreddits(args);
 
-        String[] searchTerms = stocksAndSubredditsSplit.get(0);
-        String[] subRedditChoice = stocksAndSubredditsSplit.get(1);
+            String[] searchTerms = stocksAndSubredditsSplit.get(0);
+            String[] subRedditChoice = stocksAndSubredditsSplit.get(1);
 
-        System.out.println(searchTerms.length);
-        for(String tickerName : searchTerms) {
-            Stock stock = new Stock(tickerName, subRedditChoice );
-            generatedStocks.add(stock);
+            System.out.println(searchTerms.length);
+            for (String tickerName : searchTerms) {
+                Stock stock = new Stock(tickerName, subRedditChoice);
+                generatedStocks.add(stock);
+            }
+
+            saveAllToFile(generatedStocks);
+            System.out.println("All operations Complete");
+            System.out.println("Run again tomorrow after Market close.");
         }
-
-        saveAllToFile(generatedStocks);
-        System.out.println("All operations Complete");
-        System.out.println("Run again tomorrow after Market close.");
+        else {
+            System.out.println("No Operations ran. Stay excellent");
+        }
     }
+
+    public static boolean isInTimeWindow(Calendar calendar){
+        boolean isBusinessDay = BusinessDayChecker.isBusinessDay(calendar);
+
+        if(!isBusinessDay){
+            System.out.println("The US- Stock Exchanges are closed today. Do you want to run anyways ? (y/n)");
+            Scanner scanner = new Scanner(System.in);
+            boolean userChoice = scanner.nextLine().equalsIgnoreCase("y");
+            scanner.close();
+            return userChoice;
+        }
+        return false;
+    }
+
+
 }
+
+
